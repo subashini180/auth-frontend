cd8e47554fe822b50ebdad1af2350c857080d68c
subashini180 <subashininatarajan1804@gmail.com>
2023-12-31 19:11:33 +0530

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Container,Button } from 'react-bootstrap'
+import "../styles/Home.css"
+
+export const Home = () => {
+  return (
+   <Container>
+      <h1>Welcome to our website</h1>
+      <p>We are here to serve you</p>
+      <Button variant="primary" type="submit">
+          Get started
+        </Button>
+
+   </Container>
+  )
+}
+export default Home
